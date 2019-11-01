49019afddd71bb59d8ff08ae3acd820432d21759
Lúcio Reis <lucio@midiacom.uff.br>
2019-11-01 10:47:51 -0300

    Quality Game Login V 1.0.2

--- qualityGame/qualityGame/urls.py
+++ qualityGame/qualityGame/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include("login.urls")),
+    path('quality-game/login/', include("login.urls")),
+    path('quality-game/main/', include("main.urls")),
 ]
